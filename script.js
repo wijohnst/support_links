09a846164c689888829481b99f89b36a1fbea618
William  Johnston <william.johnston@Cbus-Air-197.local>
2020-07-21 15:37:06 -0400

    added sort-select

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 console.log('Welcome to Candid Support Links')
 
 let linksListNode = document.getElementById('links-list');
+let sortSelectNode = document.getElementById('sort-select')
+let allTags = [];
 
-$.getJSON("./links_data.json", (linksData) => {
+$.getJSON("./links_data.json", async (linksData) => {
     document.getElementById('links-list')
     
     
     linksData.links.map((link,index) =>{
         console.log('Rendering links...')
+        
+
         let supportLink = document.createElement('li')
         let anchor = document.createElement('a')
         anchor.href= link.url
@@ -15,7 +19,23 @@ $.getJSON("./links_data.json", (linksData) => {
         supportLink.appendChild(anchor)
         linksListNode.appendChild(supportLink)
 
+        allTags.push(link.tags);
     })
+}).then( () => {
+    let uniqueTags = [...new Set(allTags.flat())] 
+    
+    uniqueTags.map((tag, index) =>{
+        console.log(tag)
+        let linkOption = document.createElement('option')
+        linkOption.value = tag 
+        linkOption.innerText = tag
+        sortSelectNode.appendChild(linkOption)
+    })
+    
 })
+
+
+
+
 
 
