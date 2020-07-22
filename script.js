84ddf7a38d2c50fbcf4a7ca96b15d17c0de3c7f7
William  Johnston <william.johnston@Cbus-Air-197.local>
2020-07-22 10:58:35 -0400

    addded sort & reset functionality

--- script.js
+++ script.js
@@ -1,41 +1,93 @@
 console.log('Welcome to Candid Support Links')
 
+
+
 let linksListNode = document.getElementById('links-list');
 let sortSelectNode = document.getElementById('sort-select')
+
 let allTags = [];
 
-$.getJSON("./links_data.json", async (linksData) => {
-    document.getElementById('links-list')
-    
-    
-    linksData.links.map((link,index) =>{
-        console.log('Rendering links...')
-        
+// let linksData;
 
-        let supportLink = document.createElement('li')
-        let anchor = document.createElement('a')
-        anchor.href= link.url
-        anchor.innerText = link.text
-        supportLink.appendChild(anchor)
-        linksListNode.appendChild(supportLink)
-
-        allTags.push(link.tags);
-    })
-}).then( () => {
-    let uniqueTags = [...new Set(allTags.flat())] 
+$.getJSON("./links_data.json",  function() {
     
-    uniqueTags.map((tag, index) =>{
-        console.log(tag)
-        let linkOption = document.createElement('option')
-        linkOption.value = tag 
-        linkOption.innerText = tag
-        sortSelectNode.appendChild(linkOption)
-    })
-    
+})
+.done( function(json){
+   render_links(json)
+   render_tags(json)
 })
 
 
+function render_links(linksObj){
+   let {links} = linksObj;
+
+      links.map(async (link,index) =>{
+         document.getElementById('links-list')  
+         let supportLink = document.createElement('li')
+         let anchor = document.createElement('a')
+         anchor.href= link.url
+         anchor.innerText = link.text
+         supportLink.appendChild(anchor)
+         linksListNode.appendChild(supportLink)
+
+         allTags.push(link.tags)
+   })
+}
+
+function render_tags(linksObj){
+   let { links } = linksObj;
+   console.log(`Rendering ${linksObj.links}`)
+   let uniqueTags = [...new Set(allTags.flat())] 
+
+   uniqueTags.map((tag, index) =>{
+      let linkOption = document.createElement('option')
+      linkOption.value = tag 
+      linkOption.innerText = tag
+      sortSelectNode.appendChild(linkOption)
+   })
+
+}
 
 
+function sortTags(){
+    console.log(sortSelectNode.value)
+
+   $.getJSON("./links_data.json", function (){
+
+   })
+   .done( function(json){
+      
+      let {links} = json;
+
+      let filteredLinks = links.filter(link =>{
+
+         let {tags} = link
+
+         if(tags.includes(sortSelectNode.value) === true){
+            return link;
+         }
+      })
+      console.log({links:filteredLinks})
+      clearList({links: filteredLinks}).then( (linksObj) => {
+         render_links(linksObj)
+      })
+   })
+}
+
+async function clearList(linksObj){
+
+   linksListNode.innerHTML = '';
+   return linksObj
+}
+
+function resetSort(){
+
+   $.getJSON('./links_data.json', function(){
+
+   })
+   .done( function(json){
+      render_links(json);
+   })
+}
 
 
